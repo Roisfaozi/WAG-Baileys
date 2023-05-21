c3f49233d67733aa9bd7dcad7481bd237526d345
Roisfaozi <roisfaozi55@gmail.com>
2023-05-21 20:13:43 +0700

    add uppload file features

--- core/core.js
+++ core/core.js
@@ -2,60 +2,74 @@ const {
   default: makeWASocket,
   DisconnectReason,
   useSingleFileAuthState,
-} = require("@adiwajshing/baileys");
+} = require('@adiwajshing/baileys')
 
-const pino = require("pino");
-const fs = require("fs");
-const { Console } = require("console");
-const path = "./core/";
-let x;
+const pino = require('pino')
+const fs = require('fs')
+const { Console } = require('console')
+const path = './core/'
+let x
 
-exports.gas = function (msg, no, to, type) {
-  const numb = no + ".json";
-  connect(numb, msg, to, type);
-};
+exports.gas = function (msg, no, to, type, file, mime) {
+  const numb = no + '.json'
+  connect(numb, msg, to, type, file, mime)
+}
 
-async function connect(sta, msg, to, type) {
-  const { state, saveState } = useSingleFileAuthState(path.concat(sta));
-
+async function connect(sta, msg, to, type, file, mime) {
+  const { state, saveState } = useSingleFileAuthState(path.concat(sta))
   const sock = makeWASocket({
     auth: state,
     defaultQueryTimeoutMs: undefined,
-    logger: pino({ level: "fatal" }),
-    browser: ["FFA", "EDGE", "1.0"],
-  });
+    logger: pino({ level: 'fatal' }),
+    browser: ['FFA', 'EDGE', '1.0'],
+  })
 
-  sock.ev.on("connection.update", (update) => {
-    const { connection, lastDisconnect, qr } = update;
+  sock.ev.on('connection.update', async (update) => {
+    const { connection, lastDisconnect, qr } = update
 
-    if (connection == "connecting") return;
+    if (connection == 'connecting') return
 
-    if (connection === "close") {
-      let statusCode = lastDisconnect.error?.output?.statusCode;
+    if (connection === 'close') {
+      let statusCode = lastDisconnect.error?.output?.statusCode
 
       if (statusCode === DisconnectReason.restartRequired) {
-        return;
+        return
       } else if (statusCode === DisconnectReason.loggedOut) {
         if (fs.existsSync(path.concat(sta))) {
-          fs.unlinkSync(path.concat(sta));
+          fs.unlinkSync(path.concat(sta))
         }
-        return;
+        return
       }
-    } else if (connection === "open") {
+    } else if (connection === 'open') {
       if (msg != null && to != null) {
         for (let x in to) {
-          const id = to[x] + "@s.whatsapp.net";
-          if (type === "chat") {
+          const id = to[x] + '@s.whatsapp.net'
+          if (type === 'chat' && !file) {
             sock.sendMessage(id, {
               text: msg,
-            });
+            })
+          } else {
+            await sock
+              .sendMessage(id, {
+                document: {
+                  url: file,
+                  caption: msg,
+                },
+                mimetype: mime,
+                fileName: file.name,
+              })
+              .then(() => {
+                sock.sendMessage(id, {
+                  text: msg,
+                })
+              })
           }
         }
       }
     }
-  });
+  })
 
-  sock.ev.on("creds.update", saveState);
+  sock.ev.on('creds.update', saveState)
 
-  return sock;
+  return sock
 }
--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const {
   default: makeWASocket,
   useSingleFileAuthState,
-} = require("@adiwajshing/baileys");
+} = require('@adiwajshing/baileys')
 
-process.setMaxListeners(0);
+process.setMaxListeners(0)
+const fileUpload = require('express-fileupload')
 
-const http = require("http");
-const express = require("express");
-const bodyParser = require("body-parser");
-const port = 9000;
-const fs = require("fs");
-const qrcode = require("qrcode");
-const pino = require("pino");
-const socketIO = require("socket.io");
+const http = require('http')
+const express = require('express')
+const bodyParser = require('body-parser')
+const port = 9000
+const fs = require('fs')
+const qrcode = require('qrcode')
+const pino = require('pino')
+const socketIO = require('socket.io')
 
-const con = require("./core/core.js");
+const con = require('./core/core.js')
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
+const app = express()
+const server = http.createServer(app)
+const io = socketIO(server)
 
 // config cors
 // const io = require("socket.io")(server, {
@@ -29,164 +30,143 @@ const io = socketIO(server);
 //   },
 // });
 
-let x;
+let x
 
-const path = "./core/";
+const path = './core/'
 
-const { body, validationResult } = require("express-validator");
+const { body, validationResult } = require('express-validator')
+app.use(
+  fileUpload({
+    createParentPath: true,
+  })
+)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-io.on("connection", (socket) => {
-  socket.on("StartConnection", async (device) => {
-    if (fs.existsSync(path.concat(device) + ".json")) {
-      socket.emit("message", "Whatsapp connected");
-      socket.emit("ready", device);
+io.on('connection', (socket) => {
+  socket.on('StartConnection', async (device) => {
+    if (fs.existsSync(path.concat(device) + '.json')) {
+      socket.emit('message', 'Whatsapp connected')
+      socket.emit('ready', device)
     } else {
       const { state, saveState } = useSingleFileAuthState(
-        path.concat(device) + ".json"
-      );
+        path.concat(device) + '.json'
+      )
 
       const sock = makeWASocket({
         printQRInTerminal: false,
         auth: state,
-        logger: pino({ level: "fatal" }),
-        browser: ["FFA", "EDGE", "1.0"],
-      });
-      sock.ev.on("connection.update", (update) => {
-        const { connection, lastDisconnect, qr, isNewLogin } = update;
+        logger: pino({ level: 'fatal' }),
+        browser: ['FFA', 'EDGE', '1.0'],
+      })
+      sock.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect, qr, isNewLogin } = update
 
         if (qr) {
           qrcode.toDataURL(qr, (err, url) => {
-            socket.emit("qr", url);
-            socket.emit("message", "QR Code received, scan please!");
-          });
+            socket.emit('qr', url)
+            socket.emit('message', 'QR Code received, scan please!')
+          })
         }
 
-        if (connection == "close") {
-          con.gas(null, device);
-          console.log(device);
-          socket.emit("message", "Whatsapp connected");
-          socket.emit("ready", device);
+        if (connection == 'close') {
+          con.gas(null, device)
+          console.log(device)
+          socket.emit('message', 'Whatsapp connected')
+          socket.emit('ready', device)
         }
-        console.log(connection);
-      });
-      sock.ev.on("creds.update", saveState);
+        console.log(connection)
+      })
+      sock.ev.on('creds.update', saveState)
     }
-  });
+  })
 
-  socket.on("LogoutDevice", (device) => {
-    if (fs.existsSync(path.concat(device) + ".json")) {
-      fs.unlinkSync(path.concat(device) + ".json");
-      console.log("logout device " + device);
+  socket.on('LogoutDevice', (device) => {
+    if (fs.existsSync(path.concat(device) + '.json')) {
+      fs.unlinkSync(path.concat(device) + '.json')
+      console.log('logout device ' + device)
 
-      socket.emit("message", "logout device " + device);
+      socket.emit('message', 'logout device ' + device)
     }
-    return;
-  });
-});
+    return
+  })
+})
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/core/home.html");
-});
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/core/home.html')
+})
 
-app.get("/device", (req, res) => {
-  res.sendFile(__dirname + "/core//device.html");
-});
+app.get('/device', (req, res) => {
+  res.sendFile(__dirname + '/core//device.html')
+})
 
-app.get("/scan/:id", (req, res) => {
-  res.sendFile(__dirname + "/core//index.html");
-});
-app.get("/buku", (req, res) => {
-  res.sendFile(__dirname + "/core//buku.html");
-});
+app.get('/scan/:id', (req, res) => {
+  res.sendFile(__dirname + '/core//index.html')
+})
+app.get('/buku', (req, res) => {
+  res.sendFile(__dirname + '/core//buku.html')
+})
+
+const wrapAsync = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next)
+}
+
+const sendResponse = (res, statusCode, status, message) => {
+  res.status(statusCode).json({ status, message })
+}
+
+const validPhoneNumber = (numbers) => {
+  return numbers.every((number) => number.length >= 12)
+}
 
 app.post(
-  "/send",
-  [
-    body("number").notEmpty(),
-    body("message").notEmpty(),
-    body("to").notEmpty(),
-    body("type").notEmpty(),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req).formatWith(({ msg }) => {
-      return msg;
-    });
-
-    if (!errors.isEmpty()) {
-      return res.status(422).json({
-        status: false,
-        message: errors.mapped(),
-      });
-    } else {
-      var number = req.body.number;
-      var to = req.body.to;
-      var type = req.body.type;
-      var msg = req.body.message;
-
-      if (fs.existsSync(path.concat(number) + ".json")) {
-        if (Array.isArray(to)) {
-          try {
-            for (let x in to) {
-              if (to[x].length < 12) {
-                throw "value number invalid, must be greater than 12 digit";
-              }
-            }
-
-            con.gas(msg, number, to, type);
-            res.writeHead(200, {
-              "Content-Type": "application/json",
-            });
-            res.end(
-              JSON.stringify({
-                status: true,
-                message: "success",
-              })
-            );
-          } catch (error) {
-            res.writeHead(401, {
-              "Content-Type": "application/json",
-            });
-            res.end(
-              JSON.stringify({
-                status: false,
-                message: error,
-              })
-            );
-          }
-        } else {
-          res.writeHead(401, {
-            "Content-Type": "application/json",
-          });
-          res.end(
-            JSON.stringify({
-              status: false,
-              message: "input type to is not array value",
-            })
-          );
-        }
+  '/send',
+  wrapAsync(async (req, res) => {
+    const {
+      body: { message: msg, number, to, type },
+      files,
+    } = req
+    const errors = validationResult(req).formatWith(({ msg }) => msg)
+    const creeds = await fs.existsSync(`${path.concat(number)}.json`)
+    if (
+      errors.isEmpty() &&
+      creeds &&
+      Array.isArray(to) &&
+      validPhoneNumber(to)
+    ) {
+      if (files) {
+        const file_ubah_nama = `${new Date().getTime()}_${
+          Object.keys(files)[0]
+        }`
+        const fileDikirim = files[Object.keys(files)[0]]
+        const namafiledikirim = `./uploads/${file_ubah_nama}`
+        const fileDikirim_Mime = fileDikirim.mimetype
+        await fileDikirim.mv(`./uploads/${file_ubah_nama}`)
+        await con.gas(msg, number, to, type, namafiledikirim, fileDikirim_Mime)
       } else {
-        res.writeHead(401, {
-          "Content-Type": "application/json",
-        });
-        res.end(
-          JSON.stringify({
-            status: false,
-            message: "Please scan the QR before use the API",
-          })
-        );
+        await con.gas(msg, number, to, type)
       }
+      sendResponse(res, 200, true, 'success')
+    } else if (!errors.isEmpty()) {
+      sendResponse(res, 422, false, errors.mapped())
+    } else if (!creeds) {
+      sendResponse(res, 401, false, 'Please scan the QR before use the API')
+    } else if (!Array.isArray(to) || !validPhoneNumber(to)) {
+      sendResponse(
+        res,
+        401,
+        false,
+        "Input type 'to' is not an array or contains invalid phone numbers"
+      )
     }
-  }
-);
+  })
+)
 
-app.post("/device", (req, res) => {
-  const no = req.body.device;
-  res.redirect("/scan/" + no);
-});
+app.post('/device', (req, res) => {
+  const no = req.body.device
+  res.redirect('/scan/' + no)
+})
 
 server.listen(port, function () {
-  console.log("App running on : " + port);
-});
+  console.log('App running on : ' + port)
+})
